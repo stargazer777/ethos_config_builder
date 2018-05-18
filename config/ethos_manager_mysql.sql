ce946f11a7b23a531c5711438af2a06fe1f59c5c
stargazer777 <22nAiYb7Gmd&mDC2N7Q@DEX%jR>
2018-05-17 22:32:07 -0400

    Updated mysql schmea

--- config/ethos_manager_mysql.sql
+++ config/ethos_manager_mysql.sql
@@ -298,10 +298,70 @@ CREATE TABLE `mining_rig` (
 -- Table structure for table `mining_site`
 --
 
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2018 at 04:01 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ethos_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mining_site`
+--
+
 CREATE TABLE `mining_site` (
   `mining_site_id` bigint(20) UNSIGNED NOT NULL,
-  `mining_site_name` varchar(120) NOT NULL
+  `mining_site_name` varchar(120) NOT NULL,
+  `custom_panel_name` char(6) NOT NULL,
+  `custom_panel_pass` char(6) NOT NULL,
+  `max_gpu_temp` int(11) NOT NULL,
+  `global_fan` int(11) NOT NULL,
+  `autoreboot` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mining_site`
+--
+ALTER TABLE `mining_site`
+  ADD PRIMARY KEY (`mining_site_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mining_site`
+--
+ALTER TABLE `mining_site`
+  MODIFY `mining_site_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 --
 -- RELATIONSHIPS FOR TABLE `mining_site`:
